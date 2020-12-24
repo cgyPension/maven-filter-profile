a1874d59788c7d281206568d87fa49bd26ca4f66
cgyPension <67064353+cgyPension@users.noreply.github.com>
2020-12-24 22:39:28 +0800

    Initial commit

---
+++ src/main/java/com/cgy/utils/PropertiesUtil.scala
@@ -0,0 +1,24 @@
+package com.cgy.utils
+
+import java.util.ResourceBundle
+
+/**
+ * 获取配置文件工具类
+ */
+object PropertiesUtil {
+
+    // 绑定配置文件
+    // ResourceBundle专门用于读取配置文件，所以读取时，不需要增加扩展名
+    // 国际化 = I18N => Properties
+    val project: ResourceBundle = ResourceBundle.getBundle("config")
+
+    def getValue( key : String ): String = {
+        project.getString(key)
+    }
+
+    def main(args: Array[String]): Unit = {
+
+        println(getValue("kafka.broker.list"))
+
+    }
+}
