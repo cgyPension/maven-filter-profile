f19c055efc3ba12334a47adbed45d2585d7d25fe
cgyPension <67064353+cgyPension@users.noreply.github.com>
2020-12-25 07:58:12 +0800

    resources增加了三个文件

--- src/main/java/com/cgy/utils/PropertiesUtil.scala
+++ src/main/java/com/cgy/utils/PropertiesUtil.scala
@@ -10,7 +10,8 @@ object PropertiesUtil {
     // 绑定配置文件
     // ResourceBundle专门用于读取配置文件，所以读取时，不需要增加扩展名
     // 国际化 = I18N => Properties
-    val project: ResourceBundle = ResourceBundle.getBundle("config")
+    //val project: ResourceBundle = ResourceBundle.getBundle("config")
+    val project: ResourceBundle = ResourceBundle.getBundle("dev/config")
 
     def getValue( key : String ): String = {
         project.getString(key)
